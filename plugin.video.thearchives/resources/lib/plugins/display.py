f0ad24b76d53c116c0a7963a70ed38564541b87c
Tribalize <Tribalize@users.noreply.github.com>
2022-10-25 04:51:44 -0400

    update

--- plugin.video.thearchives/resources/lib/plugins/display.py
+++ plugin.video.thearchives/resources/lib/plugins/display.py
@@ -12,6 +12,14 @@ class display(Plugin):
     def display_list(self, jen_list):
         display_list = [(route_plugin.url_for_path(item["link"]), item["list_item"], item["is_dir"]) for item in jen_list]    	
         addDirectoryItems(route_plugin.handle, display_list, len(display_list))
-        setContent(int(sys.argv[1]), 'videos') 
+        content = { item.get("content") for item in jen_list }
+        category = "movies"
+        if content == { "video" }:
+            category = "videos"
+        elif content == { "tvshow"} or content == {"season"}:
+            category = "tvshows"
+        elif content == { "episode"}:
+            category = "episodes"
+        setContent(int(sys.argv[1]),  category) 
         endOfDirectory(route_plugin.handle)
         return True
